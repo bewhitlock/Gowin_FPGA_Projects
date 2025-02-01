6e600121cc4d9083274efc2f2e9a4831ec141d10
Ben Whitlock <Bewhitlock@icloud.com>
2025-02-01 12:47:04 -0600

    commit

--- projects/vga/camera/i2c.v
+++ projects/vga/camera/i2c.v
@@ -17,15 +17,10 @@ reg scl_clk; //pulses in between sda_clk
 reg[5:0] send_count;
 reg sda_reg;
 reg scl_reg;
-assign sda = sda_reg? 1'b1 : 1'b0; //pullup on resistor, change z to 1 to test
-//always @(*) begin
-    //if (6'd38 < send_count < 6'd2) begin
-        //scl = scl_reg;
-    //end else begin
-        //scl = scl_clk;
-    //end
-//end
-assign scl = ( (6'd2 > send_count) | (send_count > 6'd38) )?scl_reg:scl_clk;
+
+assign sda = sda_reg? 1'bz : 1'b0; //pullup on resistor, change z to 1 to test
+
+assign scl = ( (6'd4 > send_count) | (send_count > 6'd39) )?scl_reg:scl_clk;
 
 
 assign done = done_reg;
@@ -68,17 +63,19 @@ always @(posedge scl_clk or negedge scl_clk or posedge reset) begin
     if (reset) begin
         ack_reg <= 1'b0;
     end else begin
-        if (send_count == 6'd11 | 6'd20 | 6'd29 | 6'd38) begin
+        if (send_count == 6'd12 | 6'd21 | 6'd30 | 6'd39) begin
             ack_reg <= (ack_reg | sda);
         end
     end
 end
 
 always @(negedge sda_clk) begin
-    if (send_count < 6'd41) begin
-        send_count <= send_count + 1'b1;
-    end else begin
-        send_count <= 6'd0;
+    if(sendit) begin
+        if (send_count < 6'd42) begin
+            send_count <= send_count + 1'b1;
+        end else begin
+            send_count <= 6'd0;
+        end
     end
 end
 always @(posedge sda_clk or posedge reset) begin
@@ -88,53 +85,61 @@ always @(posedge sda_clk or posedge reset) begin
         if(sendit) begin
             case(send_count)
                 0: begin
-                    sda_reg <= 1'b0;
+                    sda_reg <= 1'b1;
                     scl_reg <= 1'b1;
                     done_reg = 1'b0;
                 end
                 1: begin
                     sda_reg <= 1'b0;
-                    scl_reg <= 1'b0;
+                    scl_reg <= 1'b1;
                 end
                 2: begin
                     sda_reg <= 1'b0;
                     scl_reg <= 1'b0;
                 end
-                3:sda_reg <= i2c_address[6];
-                4:sda_reg <= i2c_address[5];
-                5:sda_reg <= i2c_address[4];
-                6:sda_reg <= i2c_address[3];
-                7:sda_reg <= i2c_address[2];
-                8:sda_reg <= i2c_address[1];
-                9:sda_reg <= i2c_address[0];
-                10:sda_reg <= 1'b0; //skip 11, ack bit
-                12:sda_reg <= send_dat[23];
-                13:sda_reg <= send_dat[22];
-                14:sda_reg <= send_dat[21];
-                15:sda_reg <= send_dat[20];
-                16:sda_reg <= send_dat[19];
-                17:sda_reg <= send_dat[18];
-                18:sda_reg <= send_dat[17];
-                19:sda_reg <= send_dat[16]; //skip 20, ack bit
-                21:sda_reg <= send_dat[15];
-                22:sda_reg <= send_dat[14];
-                23:sda_reg <= send_dat[13];
-                24:sda_reg <= send_dat[12];
-                25:sda_reg <= send_dat[11];
-                26:sda_reg <= send_dat[10];
-                27:sda_reg <= send_dat[9];
-                28:sda_reg <= send_dat[8]; //skip 29, ack bit
-                30:sda_reg <= send_dat[7];
-                31:sda_reg <= send_dat[6];
-                32:sda_reg <= send_dat[5];
-                33:sda_reg <= send_dat[4];
-                34:sda_reg <= send_dat[3];
-                35:sda_reg <= send_dat[2];
-                36:sda_reg <= send_dat[1];
-                37:sda_reg <= send_dat[0]; //skip 38, ack bit
-                39:sda_reg <= 1'b0;
-                40:scl_reg <= 1'b1;
-                41:begin 
+                3: begin
+                    sda_reg <= 1'b0;
+                    scl_reg <= 1'b0;
+                end
+                4:sda_reg <= i2c_address[6];
+                5:sda_reg <= i2c_address[5];
+                6:sda_reg <= i2c_address[4];
+                7:sda_reg <= i2c_address[3];
+                8:sda_reg <= i2c_address[2];
+                9:sda_reg <= i2c_address[1];
+                10:sda_reg <= i2c_address[0];
+                11:sda_reg <= 1'b0; // r/w bit
+                12:sda_reg <= 1'b1; //1 for z
+                13:sda_reg <= send_dat[23];
+                14:sda_reg <= send_dat[22];
+                15:sda_reg <= send_dat[21];
+                16:sda_reg <= send_dat[20];
+                17:sda_reg <= send_dat[19];
+                18:sda_reg <= send_dat[18];
+                19:sda_reg <= send_dat[17];
+                20:sda_reg <= send_dat[16]; //skip 21, ack bit
+                21:sda_reg <= 1'b1; //1 for z
+                22:sda_reg <= send_dat[15];
+                23:sda_reg <= send_dat[14];
+                24:sda_reg <= send_dat[13];
+                25:sda_reg <= send_dat[12];
+                26:sda_reg <= send_dat[11];
+                27:sda_reg <= send_dat[10];
+                28:sda_reg <= send_dat[9];
+                29:sda_reg <= send_dat[8]; //skip 30, ack bit
+                30:sda_reg <= 1'b1; //1 for z
+                31:sda_reg <= send_dat[7];
+                32:sda_reg <= send_dat[6];
+                33:sda_reg <= send_dat[5];
+                34:sda_reg <= send_dat[4];
+                35:sda_reg <= send_dat[3];
+                36:sda_reg <= send_dat[2];
+                37:sda_reg <= send_dat[1];
+                38:sda_reg <= send_dat[0]; //skip 39, ack bit
+                39:sda_reg <= 1'b1; //1 for z
+                40:sda_reg <= 1'b0;
+                41:scl_reg <= 1'b1;
+                42:begin 
                     sda_reg <= 1'b1;
                     done_reg <= 1'b1;
                 end
--- projects/vga/camera/i2c_tb.v
+++ projects/vga/camera/i2c_tb.v
@@ -1,26 +1,37 @@
 module i2c_tb ();
 
+wire done;
 wire meg25;
 wire sda;
 wire scl;
+wire ack;
 wire[23:0] send_dat;
 wire [6:0] send_count;
 reg sendit;
 reg meg_reg25;
 assign meg25 = meg_reg25;
-assign send_dat = 24'd16777215;
-i2c dude (.scl(scl), .sda(sda), .meg25(meg25), .send_dat(send_dat), .sendit(sendit), .send_count_out(send_count));
+assign send_dat = 24'b111111111111111111111111;
+i2c dude (.scl(scl), .sda(sda), .meg25(meg25), .send_dat(send_dat), .sendit(sendit), .send_count_out(send_count), .ack(ack), .done(done));
 
 initial begin
 $dumpfile("test.vcd");
-$dumpvars(0, sda, scl, sendit, send_count);
+$dumpvars(0, sda, scl, sendit, send_count, ack, done);
 meg_reg25 = 1'b0;
-#10 sendit = 1'b1;
-#15000;
+sendit = 1'b0;
+#2000 sendit = 1'b1;
+#25000;
 $finish;
 end
 
 always begin
     #1 meg_reg25 = ~meg_reg25;
+end
+
+always @(done) begin
+    if(done) begin
+        sendit = 1'b0;
+    end else begin
+        #2000 sendit = 1'b1;
+    end
 end
 endmodule

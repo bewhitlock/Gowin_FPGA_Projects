ac7da8c2fd95cd684865cd962b36e485c2394219
Ben Whitlock <Bewhitlock@icloud.com>
2025-02-08 15:08:57 -0600

    commit

--- projects/vga/vga_driver/vga_tb.v
+++ projects/vga/vga_driver/vga_tb.v
@@ -2,15 +2,15 @@ module vga_tb ();
 reg clk;
 wire hsync;
 wire vsync;
-wire[7:0] red;
-wire[7:0] green;
-wire[7:0] blue;
-wire[9:0] x_val;
-wire[9:0] y_val;
-wire[11:0] h_count;
-wire[11:0] v_count;
+//wire[7:0] red;
+//wire[7:0] green;
+//wire[7:0] blue;
+//wire[9:0] x_val;
+//wire[9:0] y_val;
+//wire[11:0] h_count;
+//wire[11:0] v_count;
 
-vga test_vga (.board_clock(clk), .hsync(hsync), .vsync(vsync), .red(red), .green(green), .blue(blue), .x_val(x_val), .y_val(y_val), .h_count_test(h_count), .v_count_test(v_count));
+vga test_vga (.board_clock(clk), .hsync(hsync), .vsync(vsync) );
 
 
 
@@ -20,7 +20,7 @@ initial begin
 end
 initial begin
     $dumpfile("vga_test.vcd");
-    $dumpvars(0, clk, hsync, vsync, x_val, v_count, h_count, y_val, red, green, blue);
+    $dumpvars(0, clk, hsync, vsync);
 end
 
 always begin
